bc6d9b63e93a76c01530bd18445bfad1520eb42e
Ling Chen <lche0147@student.monash.edu>
2023-08-31 12:46:03 +1000

    latest working build for both PC and Pi, without path planning

---
+++ ROS_Pi/build/robot_controller/build/lib/robot_controller/ultrasonic.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+import RPi.GPIO as GPIO          
+import logging
+import time
+from robot_interfaces.msg import Distances
+
+class Ultrasonic(Node):
+    def __init__(self):
+        super().__init__("ultrasonic_node") # name of the node in ros2
+        time.sleep(1)
+        self.get_logger().info("Ultrasonic class initiallised")
+
+        #GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
+        
+        #set GPIO Pins
+        self.GPIO_TRIGGER = 27
+        self.GPIO_ECHO = 17
+        
+        #set GPIO direction (IN / OUT)
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+
+        self.GPIO_TRIGGER_2 = 2
+        self.GPIO_ECHO_2 = 3
+
+        GPIO.setup(self.GPIO_TRIGGER_2, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO_2, GPIO.IN)
+
+        time.sleep(2)
+
+        self.measure_timer = self.create_timer(0.2, self.measure_distance)
+        self.measure_publisher = self.create_publisher(Distances, "ultrasonic_distances", 10) # msg type, topic_name to publish to, buffer size
+
+
+    def measure_distance(self):
+        msg = Distances()
+        msg.sensor1 = float(self.get_distance(1))
+        time.sleep(0.01)
+        msg.sensor2 = float(self.get_distance(2))
+        time.sleep(0.01)
+        msg.sensor3 = float(self.get_distance(2))
+        self.measure_publisher.publish(msg)
+
+
+    def get_distance(self, sensor):
+        if sensor == 1:
+            trig = self.GPIO_TRIGGER
+            echo = self. GPIO_ECHO
+        elif sensor ==2:
+            trig = self.GPIO_TRIGGER_2
+            echo = self. GPIO_ECHO_2
+
+        # set Trigger to HIGH
+        GPIO.output(trig, True)
+    
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(trig, False)
+    
+        StartTime = time.time()
+        StopTime = time.time()
+    
+        no_echo = False
+        # save StartTime
+        start_start_time = StartTime
+        while GPIO.input(echo) == 0:
+            if time.time() - start_start_time > 0.2: 
+                break
+            StartTime = time.time()
+    
+        if not no_echo:
+            # save time of arrival
+            while GPIO.input(echo) == 1:
+                StopTime = time.time()
+    
+    
+        
+            # time difference between start and arrival
+            TimeElapsed = StopTime - StartTime
+            # multiply with the sonic speed (34300 cm/s)
+            # and divide by 2, because there and back
+            # self.get_logger().info("time between emission and detection: " + str(TimeElapsed))
+            distance = (TimeElapsed * 34300) / 2
+            # self.get_logger().info("distance recorded: " + str(distance))
+            if distance >= 200:
+                return -999.0
+            return distance
+        else:
+            return -999.0
+
+    
+    def test_continuous_reading(self):
+        # self.get_logger().info("Ultrasonic would return distance continuously")
+        try:
+            while True:
+                dist = self.get_distance()
+                self.get_logger().info("Measured Distance =" + str(dist) +" cm")
+                time.sleep(0.1)
+            # Reset by pressing CTRL + C
+        except KeyboardInterrupt:
+            self.get_logger().warning("Measurement stopped by User")
+            GPIO.cleanup()
+
+
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+        ultrasonic_node = Ultrasonic()
+        rclpy.spin(ultrasonic_node)
+
+    except KeyboardInterrupt:
+        ultrasonic_node.get_logger().info("drive node shutdown")
+    rclpy.shutdown()
+
+
+if __name__ == "__main__": # If you want to run node from terminal directly
+    main()
---
+++ ROS_Pi/install/robot_controller/lib/python3.10/site-packages/robot_controller/encoder.py
@@ -0,0 +1,154 @@
+import RPi.GPIO as GPIO
+from rclpy.node import Node
+import rclpy
+import time
+from robot_interfaces.msg import EncoderInfo
+
+class Encoder(Node):
+    def __init__(self):
+        super().__init__("encoder_node")
+        self.left_count = 0
+        self.right_count = 0
+        self.left_state = '00'
+        self.right_state = '00'
+        self.left_vel = 0
+        self.right_vel = 0
+
+        self.left_speed = 85
+        self.right_speed = 85
+        self.prev_error = 0
+        self.error_sum = 0
+
+        GPIO.setmode(GPIO.BCM)
+        self.left_wheel_ena = 5
+        self.left_wheel_enb = 6
+        GPIO.setup(self.left_wheel_ena, GPIO.IN)
+        GPIO.setup(self.left_wheel_enb, GPIO.IN)
+
+
+        self.right_wheel_ena = 16
+        self.right_wheel_enb = 26
+        GPIO.setup(self.right_wheel_ena, GPIO.IN)
+        GPIO.setup(self.right_wheel_enb, GPIO.IN)
+
+        self.encoder_publisher = self.create_publisher(EncoderInfo, "encoder_info", 10) # msg type, topic_name to publish to, buffer size
+
+
+        
+        # Setting up GPIO
+        time.sleep(2)
+        GPIO.setmode(GPIO.BCM)
+        self.en = 25
+        GPIO.setup(self.en,GPIO.OUT)
+        self.p1=GPIO.PWM(self.en,1000)
+        self.p1.start(0)
+        self.p1.ChangeDutyCycle(85)
+
+        self.en2 = 1
+        GPIO.setup(self.en2,GPIO.OUT)
+        self.p2=GPIO.PWM(self.en2,1000)
+        self.p2.start(0)
+        self.p2.ChangeDutyCycle(85)
+
+        self.get_logger().info("Encoder node initialised")
+
+    def detectEncoder(self):
+        # sample_freq = 3000
+        # period = 1/sample_freq
+        # t = time.time()
+
+        reset_time = True
+        while True:
+            # t += period
+            # track time
+            if reset_time:
+                start_time = time.time()
+                reset_time = False
+
+                prev_left_count = self.left_count
+                prev_right_count = self.right_count
+
+
+            # check encoder
+            lp1 = GPIO.input(self.left_wheel_ena)
+            lp2 = GPIO.input(self.left_wheel_enb)
+            left_newState = "{}{}".format(lp1, lp2)
+
+            if self.left_state != left_newState:
+                self.left_count += 1
+                self.left_state = left_newState
+
+            rp1 = GPIO.input(self.right_wheel_ena)
+            rp2 = GPIO.input(self.right_wheel_enb)
+            right_newState = "{}{}".format(rp1, rp2)
+
+            if self.right_state != right_newState:
+                self.right_count += 1
+                self.right_state = right_newState
+
+            # calc velocity
+            elapsed_time = time.time() - start_time 
+            if (elapsed_time >= 0.2):
+
+                self.left_vel = (self.left_count - prev_left_count)/elapsed_time #vel in cps, counts per sec
+                self.right_vel = (self.right_count - prev_right_count)/elapsed_time
+
+                try:
+                    error = 100 * (self.left_vel - self.right_vel)/self.left_vel
+                except ZeroDivisionError:
+                    error = 0
+
+                if abs(error) >= 1 and abs(error) < 50:
+                    self.correct_speed(error)
+
+
+                reset_time = True
+            # publish msg
+            msg = EncoderInfo()
+            msg.left_count = int(self.left_count)
+            msg.right_count = int(self.right_count)
+            self.encoder_publisher.publish(msg)
+            # self.get_logger().info("COUNT_ENC: (" + str(msg.left_count) + ", " + str(msg.right_count) + ")")
+
+            # time.sleep(max(0,t-time.time()))
+
+    def correct_speed(self, error):
+        """
+        This function will adjust the right wheel speed so that it matches the left speed
+        """
+        
+        KP = 0.05
+        KD = 0.015
+        KI = 0.005
+
+        self.get_logger().info("error: " + str(error))
+        new_speed = self.right_speed + (KP*error) + (KD*self.prev_error) + (KI*self.error_sum)
+        if new_speed < 0 or  new_speed > 100:
+            self.get_logger().error("Invalid Speed of: " + str(new_speed))
+            raise Exception("Invalid Speed of: " + str(new_speed))
+        
+        self.right_speed = new_speed
+        self.p1.ChangeDutyCycle(new_speed)
+        self.get_logger().info("right wheel speed adjusted to: " + str(self.right_speed))
+
+        self.prev_error = error
+        self.error_sum += error
+
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+
+        encoder_node = Encoder()
+        encoder_node.detectEncoder()
+        rclpy.spin(encoder_node)
+
+    except KeyboardInterrupt:
+        encoder_node.clear_gpio()
+        encoder_node.get_logger().info("Encoder node shutdown")
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
+
