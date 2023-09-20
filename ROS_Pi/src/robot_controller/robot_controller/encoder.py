1f23dd42d8bc9657d97ee699242ffd9d49ef3953
Ling Chen <lche0147@student.monash.edu>
2023-09-20 11:06:37 +1000

    plot

--- ROS_Pi/src/robot_controller/robot_controller/encoder.py
+++ ROS_Pi/src/robot_controller/robot_controller/encoder.py
@@ -146,16 +146,17 @@ class Encoder(Node):
 
             # time.sleep(max(0,t-time.perf_counter()))
 
-            # if time.perf_counter() - self.start_graph_time >= 30:
-            #     self.get_logger().info("graph done")
-            #     plt.plot(self.left_speed_arr)
-            #     plt.legend(["left"])
-            #     plt.savefig('left_encoder.png')
-            #     plt.close()
-            #     plt.figure()
-            #     plt.plot(self.right_speed_arr)
-            #     plt.legend(["right"])
-            #     plt.savefig("right_encoder.png")
+            if time.perf_counter() - self.start_graph_time >= 30:
+                self.get_logger().info("graph done")
+                plt.plot(self.left_speed_arr)
+                plt.legend(["left"])
+                plt.savefig('left_encoder.png')
+                plt.close()
+                plt.figure()
+                plt.plot(self.right_speed_arr)
+                plt.legend(["right"])
+                plt.savefig("right_encoder.png")
+                self.start_graph_time = time.perf_counter() + 999999
 
 
     def correct_speed(self, motor, error):
