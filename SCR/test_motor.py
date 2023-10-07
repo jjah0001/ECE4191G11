3412ccd84d2fc79279c14970fde08626df81718b
Ling Chen <lche0147@student.monash.edu>
2023-10-07 15:46:03 +1100

    led broll

--- SCR/test_motor.py
+++ SCR/test_motor.py
@@ -1,7 +1,15 @@
 from motor import Motor
 import time
+from led import LED
+
 
 motor = Motor()
+led = LED()
 
-motor.drive_forwards(3.5)
+motor.drive_forwards(2)
+motor.stop()
+led.turn_on()
+time.sleep(3)
+led.turn_off()
+motor.drive_forwards(5)
 motor.stop()
